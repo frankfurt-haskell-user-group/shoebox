4f21134eee49802b69ffb265566629e9b7cf31d5
Urs of the Backwoods <uotbw@hgamer3d.org>
2017-08-02 09:05:48 +0200

    updated buttons in file row, layout

--- frontend/src/Bootstrap.purs
+++ frontend/src/Bootstrap.purs
@@ -1,6 +1,9 @@
 module Bootstrap where
 
 import Halogen.HTML (ClassName(..))
+import Halogen.HTML.Core (AttrName(..))
+import Halogen.HTML.Properties (attr)
+import Data.Maybe (Maybe(..))
 
 container :: ClassName
 container = ClassName "container"
@@ -86,6 +89,36 @@ buttonDanger = ClassName "btn btn-danger"
 buttonLink :: ClassName
 buttonLink = ClassName "btn btn-link"
 
+buttonSmall :: ClassName
+buttonSmall = ClassName "btn-sm"
+
+buttonExtraSmall :: ClassName
+buttonExtraSmall = ClassName "btn-xs"
+
+buttonLarge :: ClassName
+buttonLarge = ClassName "btn-lg"
+
+
+glyphiconSend :: ClassName
+glyphiconSend = ClassName "glyphicon glyphicon-send"
+
+glyphiconOpen :: ClassName
+glyphiconOpen = ClassName "glyphicon glyphicon-open"
+
+glyphiconSave :: ClassName
+glyphiconSave = ClassName "glyphicon glyphicon-save"
+
+glyphiconPlusSign :: ClassName
+glyphiconPlusSign = ClassName "glyphicon glyphicon-plus-sign"
+
+glyphiconRemoveSign :: ClassName
+glyphiconRemoveSign = ClassName "glyphicon glyphicon-remove-sign"
+
+
+
+
+
+
 
 {-
 Button Dropdown
@@ -121,11 +154,71 @@ dropdownMenu = ClassName "dropdown-menu"
 caret :: ClassName
 caret = ClassName "caret"
 
+{-
+
+      HH.div [HP.class_ HB.row] [
+        HH.div [HP.class_ HB.colSm3] [   -- menu section
+          HH.div [HP.class_ (ClassName "btn-group-vertical")] [
+           HH.button
+              [ HP.disabled $ st.word == mempty
+              , HE.onClick $ HE.input_ QueryWord
+              ]
+              [ HH.text "Send " , HH.span [ HP.class_ (ClassName "glyphicon glyphicon-send")] [] ]
+          , HH.button
+              [ HP.disabled $ st.word == mempty
+              , HE.onClick $ HE.input_ QueryWord
+              ]
+              [ HH.text "Send " , HH.span [ HP.class_ (ClassName "glyphicon glyphicon-send")] [] ]
+          ]
+        ],
+
+        HH.div [HP.class_ HB.colSm9] [   -- content section
+
+
+        HH.div [HP.class_ (ClassName "dropdown")] [
+          HH.button [ HP.class_ (ClassName "btn btn-secondary dropdown-toggle"), 
+                      HP.attr (AttrName "type") "button", 
+                      HP.attr (AttrName "id") "dropdownMenuButton" ,
+                      HP.attr (AttrName "data-toggle") "dropdown", 
+                      HP.attr (AttrName "aria-haspopup") "true", 
+                      HP.attr (AttrName "aria-expanded") "false"
+                      ] 
+                      [
+                        HH.text "drop this" 
+                      ]
+          , HH.ul [HP.class_ HB.dropdownMenu, HP.attr (AttrName "aria-labelledby") "dropdownMenuButton"] [
+              HH.li [] [ HH.a [HP.class_ (ClassName "dropdown-item"), HP.attr (AttrName "href") "#"] [HH.text "wow"] ] ,
+              HH.li [] [ HH.a [HP.class_ (ClassName "dropdown-item"), HP.attr (AttrName "href") "#"] [HH.text "wow2"] ] 
+              ]
+          ]
+
+        , HH.p [][]
+      
+        , HH.input
+            [ HP.value st.word
+            , HE.onValueInput $ HE.input UpdateWord
+            ]
+        , HH.button
+            [ HP.disabled $ st.word == mempty
+            , HE.onClick $ HE.input_ QueryWord
+            ]
+            [ HH.text "Send " , HH.span [ HP.class_ (ClassName "glyphicon glyphicon-send")] [] ]
+        , HH.p_ [ HH.text st.segmentation ]
+        , HH.p_ [ HH.text st.gloss ]
+
+        ]
+      ],
+
+      -- bottom row 
+      HH.div [HP.class_ (ClassName "row")] [
+        HH.div [HP.class_ (ClassName "col-sm-12")] [
+          HH.text "Shoebox (c) 2017 - Frankfurt Haskell User Group"
+        ]
+      ]
 
 
 
-
-
+-}
 
 
 
--- frontend/src/GlossingPage.purs
+++ frontend/src/GlossingPage.purs
@@ -4,7 +4,7 @@ module GlossingPage
   ) where
 
 import Prelude
-import Control.Monad.Aff (Aff, makeAff)
+import Control.Monad.Aff (Aff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Maybe (Maybe(Nothing))
 import Data.Monoid (mempty)
@@ -18,13 +18,12 @@ import Network.HTTP.Affjax (AJAX)
 import Node.ChildProcess (ChildProcess, CHILD_PROCESS, stdout, stdin)
 import Node.Stream (write, read, uncork, onData)
 import Node.Encoding (Encoding(..))
-import Data.Maybe
-import Control.Monad.Eff.Class
+import Data.Maybe (Maybe(..))
 import Node.Buffer (fromString, toString, BUFFER)
 import Halogen.HTML (ClassName(..))
 import Bootstrap as HB
 
-import Api
+import Api (queryAPI)
 
 type Word = String
 type Choice = Array
@@ -39,6 +38,10 @@ type State = { word         :: Word
 
 data Query a = UpdateWord Word a
              | QueryWord a
+             | OpenDatabase a
+             | SaveDatabase a
+             | NewDatabase a
+             | RemoveDatabase a
 
 ui :: forall eff. ChildProcess -> H.Component HH.HTML Query Unit Void (Aff (exception :: EXCEPTION, cp::CHILD_PROCESS, buffer::BUFFER  | eff))
 ui cpIn = H.component
@@ -56,75 +59,61 @@ ui cpIn = H.component
                    }
 
     render :: State -> H.ComponentHTML Query
-    render st = HH.div [HP.class_ (ClassName "container")] [  
+    render st =  
 
-    -- outer container, fluid (see bootstrap)
-      -- top row 
-      HH.div [HP.class_ (ClassName "row")] [
-        HH.div [HP.class_ (ClassName "col-sm-12")] [
-          HH.h3_ [ HH.small_ [HH.text "selected database: " ], HH.text "frz" ]
-        ]
-      ],
+      -- outer container, fluid (see bootstrap)
+      HH.div [HP.class_ (ClassName "container")] [  
 
-      -- middle row
-      HH.div [HP.class_ HB.row] [
-        HH.div [HP.class_ HB.colSm3] [   -- menu section
-          HH.div [HP.class_ (ClassName "btn-group-vertical")] [
-           HH.button
-              [ HP.disabled $ st.word == mempty
-              , HE.onClick $ HE.input_ QueryWord
-              ]
-              [ HH.text "Send " , HH.span [ HP.class_ (ClassName "glyphicon glyphicon-send")] [] ]
-          , HH.button
-              [ HP.disabled $ st.word == mempty
-              , HE.onClick $ HE.input_ QueryWord
-              ]
-              [ HH.text "Send " , HH.span [ HP.class_ (ClassName "glyphicon glyphicon-send")] [] ]
+        -- top row 
+        HH.div [HP.classes [HB.row, (ClassName "top-row")]] [
+          -- left side, db text
+          HH.div [HP.classes [HB.colSm6]] [
+            HH.h3_ [ HH.small_ [ HH.text "selected database: " ], HH.text "frz"]
+          ]
+
+          , HH.div [HP.classes [HB.colSm6, (ClassName "file-buttons")]] [
+                  HH.button
+                    [ HP.classes [HB.buttonPrimary, HB.buttonSmall]
+                      , HE.onClick $ HE.input_ OpenDatabase
+                    ]
+                    [ HH.text "Open " , HH.span [ HP.classes [HB.glyphiconOpen] ] [] ]
+                  , HH.text " "
+                  , HH.button
+                    [ HP.classes [HB.buttonDefault, HB.buttonSmall]
+                      , HE.onClick $ HE.input_ SaveDatabase
+                    ]
+                    [ HH.text "Save " , HH.span [ HP.classes [HB.glyphiconSave] ] [] ]
+                  , HH.text " "
+                  , HH.button
+                    [ HP.classes [HB.buttonDefault, HB.buttonSmall]
+                      , HE.onClick $ HE.input_ NewDatabase
+                    ]
+                    [ HH.text "New " , HH.span [ HP.classes [HB.glyphiconPlusSign] ] [] ]
+                  , HH.text " "
+                  , HH.button
+                    [ HP.classes [HB.buttonDanger, HB.buttonSmall]
+                      , HE.onClick $ HE.input_ RemoveDatabase
+                    ]
+                    [ HH.text "Delete " , HH.span [ HP.classes [HB.glyphiconRemoveSign] ] [] ]
+          ]
+
+        ],
+
+        -- middle row
+        HH.div [HP.classes [HB.row, (ClassName "middle-row")]] [
+          HH.div [HP.classes [HB.colSm12]] [
+
           ]
         ],
 
-        HH.div [HP.class_ HB.colSm9] [   -- content section
-
-        HH.div [HP.class_ (ClassName "dropdown")] [
-          HH.button [ HP.class_ (ClassName "btn btn-secondary dropdown-toggle"), 
-                      HP.attr (AttrName "type") "button", 
-                      HP.attr (AttrName "id") "dropdownMenuButton" ,
-                      HP.attr (AttrName "data-toggle") "dropdown", 
-                      HP.attr (AttrName "aria-haspopup") "true", 
-                      HP.attr (AttrName "aria-expanded") "false"
-                      ] 
-                      [
-                        HH.text "drop this" 
-                      ]
-          , HH.ul [HP.class_ HB.dropdownMenu, HP.attr (AttrName "aria-labelledby") "dropdownMenuButton"] [
-              HH.li [] [ HH.a [HP.class_ (ClassName "dropdown-item"), HP.attr (AttrName "href") "#"] [HH.text "wow"] ] ,
-              HH.li [] [ HH.a [HP.class_ (ClassName "dropdown-item"), HP.attr (AttrName "href") "#"] [HH.text "wow2"] ] 
-              ]
+        -- bottom row
+        HH.div [HP.classes [HB.row, (ClassName "bottom-row")]] [
+          HH.div [HP.classes [HB.colSm12]] [
+            HH.text "Shoebox (c) 2017 - Frankfurt Haskell User Group"
           ]
-
-        , HH.p [][]
-      
-        , HH.input
-            [ HP.value st.word
-            , HE.onValueInput $ HE.input UpdateWord
-            ]
-        , HH.button
-            [ HP.disabled $ st.word == mempty
-            , HE.onClick $ HE.input_ QueryWord
-            ]
-            [ HH.text "Send " , HH.span [ HP.class_ (ClassName "glyphicon glyphicon-send")] [] ]
-        , HH.p_ [ HH.text st.segmentation ]
-        , HH.p_ [ HH.text st.gloss ]
-
         ]
-      ],
 
-      -- bottom row 
-      HH.div [HP.class_ (ClassName "row")] [
-        HH.div [HP.class_ (ClassName "col-sm-12")] [
-          HH.text "Shoebox (c) 2017 - Frankfurt Haskell User Group"
-        ]
-      ]
+
       ] -- outer container
 
     eval :: Query ~> H.ComponentDSL State Query Void (Aff (exception :: EXCEPTION, cp::CHILD_PROCESS, buffer::BUFFER  | eff))
@@ -136,6 +125,18 @@ ui cpIn = H.component
         pr <- H.gets _.process
         s <- H.liftAff $ queryAPI pr cmd
         H.modify $ _ {gloss = s}
+        pure next
+    eval (OpenDatabase next) = do
+--        H.liftAff $ log("open database pressed")
+        pure next
+    eval (SaveDatabase next) = do
+--        H.liftAff $ log("open database pressed")
+        pure next
+    eval (NewDatabase next) = do
+--        H.liftAff $ log("open database pressed")
+        pure next
+    eval (RemoveDatabase next) = do
+--        H.liftAff $ log("open database pressed")
         pure next
 
       {-
